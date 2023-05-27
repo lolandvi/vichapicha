8f0f7c9d60a366f24683b207a9173c87773760c9
Виктория <ivikulka@bk.ru>
2023-05-27 17:11:50 +0700

    Вывод даты

---
+++ lab8.js
@@ -0,0 +1,24 @@
+function showDate() {
+    let out = document.getElementById('current-date');
+    let today = new Date();
+    for (let k = 1; k < 6; k++) {
+        let out = document.getElementById('current-date' + k);
+        if (k == 1) {
+            out.innerHTML = 'Текущие дата время для русской локали: ' + today.toLocaleString('ru-RU');
+        }
+        else if (k == 2) {
+            out.innerHTML = 'Текущие дата время для норвежской локали: ' + today.toLocaleString('nn-NO');
+        }
+        else if (k == 3) {
+            out.innerHTML = 'Текущие дата время для американской локали: ' + today.toLocaleString('en-US');
+        }
+        else if (k == 4) {
+            out.innerHTML = 'Текущие дата время для французской локали: ' + today.toLocaleString('fr-FR');
+        }
+        else if (k == 5) {
+            out.innerHTML = 'Текущие дата время для итальянской локали: ' + today.toLocaleString('it-IT');
+        }
+    }
+}
+
+
