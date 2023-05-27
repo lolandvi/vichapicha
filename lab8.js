bfb735ba6765d6739e8a5007d54523a18c9bfff7
Виктория <ivikulka@bk.ru>
2023-05-27 17:19:45 +0700

    Вывод даты+

--- lab8.js
+++ lab8.js
@@ -4,19 +4,19 @@ function showDate() {
     for (let k = 1; k < 6; k++) {
         let out = document.getElementById('current-date' + k);
         if (k == 1) {
-            out.innerHTML = 'Текущие дата время для русской локали: ' + today.toLocaleString('ru-RU');
+            out.innerHTML = 'Текущие дата и время для русской локали: ' + today.toLocaleString('ru-RU');
         }
         else if (k == 2) {
-            out.innerHTML = 'Текущие дата время для норвежской локали: ' + today.toLocaleString('nn-NO');
+            out.innerHTML = 'Текущие дата и время для норвежской локали: ' + today.toLocaleString('nn-NO');
         }
         else if (k == 3) {
-            out.innerHTML = 'Текущие дата время для американской локали: ' + today.toLocaleString('en-US');
+            out.innerHTML = 'Текущие дата и время для американской локали: ' + today.toLocaleString('en-US');
         }
         else if (k == 4) {
-            out.innerHTML = 'Текущие дата время для французской локали: ' + today.toLocaleString('fr-FR');
+            out.innerHTML = 'Текущие дата и время для французской локали: ' + today.toLocaleString('fr-FR');
         }
         else if (k == 5) {
-            out.innerHTML = 'Текущие дата время для итальянской локали: ' + today.toLocaleString('it-IT');
+            out.innerHTML = 'Текущие дата и время для итальянской локали: ' + today.toLocaleString('it-IT');
         }
     }
 }
