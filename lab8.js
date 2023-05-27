d59d21fffece1250b5a4c69eca23210f6a0232da
Виктория <ivikulka@bk.ru>
2023-05-28 00:04:28 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -38,3 +38,10 @@ function clean() {
     out.innerHTML = '';
 }
 
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+setInterval(showTime, 1000);
+
